1ad93b54475737c44dc29b72023cf75eb0e1b6b7
siddheshlaud <sidd.laud@gmail.com>
2020-05-07 14:43:24 +0530

    Final Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +9,79 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  forbiddenNamesArr=['Chris', 'Anna'];
 
   signupForm: FormGroup;
 
   ngOnInit() {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]), //bind(this) is required as Angular will be executing the validator and we are using this in code
+        'email': new FormControl(null, [Validators.required, Validators.email],[this.forbiddenEmailAsync]) //1. Defaults, 2. Normal Validators (Sync), 3. Async Validators
       }),
-      'gender': new FormControl('female')
+      'gender': new FormControl('female'),
+      'hobbies': new FormArray([])
     });
+
+    this.signupForm.valueChanges.subscribe(
+      (value) => {
+        console.log(value);
+      }
+    );
+
+    this.signupForm.statusChanges.subscribe(
+      (status) => {
+        console.log(status);
+      }
+    );
+
+    // this.signupForm.setValue({
+    //   'userData': {
+    //     'username': 'Siddhesh',
+    //     'email': 'abc@xyz.com'
+    //   },
+    //   'gender': 'male',
+    //   'hobbies': []
+    // });
+    // this.signupForm.patchValue({
+    //   'userData': {
+    //     'username': 'Sidd'
+    //   }
+    // });
   }
 
   onSubmit() {
+    this.signupForm.reset();
     console.log(this.signupForm);
   }
+
+  onAddHobby() {
+    const hobby = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get('hobbies')).push(hobby);
+  }
+
+  //{[s:string]: boolean} JSON object of type where key will be of data type string and value of boolean
+  forbiddenNames(control: FormControl): {[s:string]: boolean} 
+  { 
+    if(this.forbiddenNamesArr.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null; // If FormControl is valid then null needs to be sent
+  }
+
+  forbiddenEmailAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject)=>{
+      setTimeout(()=>{
+        if(control.value === 'test@test.com') {
+          resolve({'emailIsForbidded': true}); // Invalid Control Value
+        } else {
+          resolve(null); // Valid control value
+        }
+      },1500);
+    });
+
+    return promise;
+  }
+
+
 }
